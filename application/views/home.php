aad34d7da1cdba482e2e085dbe391d1ace8cb203
dev1 <dev@octopuscart.com>
2019-03-28 20:25:56 +0530

    change

--- application/views/home.php
+++ application/views/home.php
@@ -529,8 +529,23 @@ for ($i = 1; $i < 7; $i++) {
             </div>
             </section>-->
 
-
-
+<hr/>
+<section class="section_offset bg_light_2">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-8 col-md-9 col-sm-8 col-lg-offset-1 appear-animation fadeInUp appear-animation-visible" data-appear-animation="fadeInUp">
+                                                    
+                                                    <img style="float: left;height: 50px;" src="<?php echo base_url(); ?>assets/images/visamaster.png"/>
+                                                    <h4 class="color_dark fw_light" style="    margin-top: 18px;
+    text-align: right;">For Manual Payment Download This Form &nbsp;&nbsp;<i class="icon-right-thin inline_select"></i></h4>
+						</div>
+						<div class="col-lg-3 col-md-3 col-sm-4 appear-animation fadeInUp appear-animation-visible" data-appear-animation="fadeInUp" data-appear-animation-delay="150" style="animation-delay: 150ms;">
+							<a href="<?php echo base_url(); ?>assets/images/Credit_Card_Authorization_Form_shaniel_fashion_ltd.jpg" download id="get_started" class="button_type_3 r_corners d_inline_b color_green tt_uppercase fs_large m_top_5 m_sm_top_25 tr_all">Download Now</a>
+						</div>
+					</div>
+				</div>
+			</section>
+<hr/>
 <?php
 $this->load->view('layout/footer');
 ?>
